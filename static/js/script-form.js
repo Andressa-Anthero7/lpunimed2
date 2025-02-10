fc6b886aaa682d5e6f4af167447999ddfefead0f
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2025-02-10 12:10:09 -0300

    deploy 10-02-2024 em dev

--- static/js/script-form.js
+++ static/js/script-form.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $(document).on('keyup','#nome, #whatsapp', function(){
         nome_digitado = $("#nome").val();
         whatsapp_digitado = $("#whatsapp").val();
+        //se nome maior que 3 letras e numero maior igual a 10 vai liberar o aceita termos
         if(nome_digitado.length >= 3 && whatsapp_digitado.length >= 10){
             $("#aceito-termos").removeAttr('disabled');
         }
@@ -11,6 +12,12 @@ $(document).ready(function(){
             whatsapp_digitado = '(' + whatsapp_digitado.slice(0, 2) + ') ' + whatsapp_digitado.slice(2);
             $("#whatsapp").val(whatsapp_digitado);
         }
+        document.getElementById("whatsapp").addEventListener("input", function (e) {
+            let v = this.value.replace(/\D/g, ""); // Remove não numéricos
+            if (v.length > 11) v = v.slice(0, 11); // Limita a 11 dígitos
+            v = v.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3"); // Formata (XX) XXXXX-XXXX
+            this.value = v;
+        });
     })
 
     $(document).on('click','#aceito-termos',function(){
